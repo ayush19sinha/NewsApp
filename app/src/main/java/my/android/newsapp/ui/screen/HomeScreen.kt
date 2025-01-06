deb0a1184945ad60d1a36bd415f5db8e3d4a0f95
ayushsinha-1 <143383811+ayushsinha-1@users.noreply.github.com>
2025-01-06 22:42:37 +0530

    Changed the ui

--- app/src/main/java/my/android/newsapp/ui/screen/HomeScreen.kt
+++ app/src/main/java/my/android/newsapp/ui/screen/HomeScreen.kt
@@ -4,7 +4,6 @@ import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -16,13 +15,15 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Tab
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -31,24 +32,22 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusRestorer
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.tv.material3.Icon
 import androidx.tv.material3.IconButton
-import androidx.tv.material3.MaterialTheme
-import androidx.tv.material3.Tab
 import androidx.tv.material3.TabRow
 import androidx.tv.material3.Text
 import my.android.newsapp.R
 import my.android.newsapp.data.model.Article
 import my.android.newsapp.ui.component.ErrorScreen
-import my.android.newsapp.ui.component.HeadlineCard
+import my.android.newsapp.ui.component.FeaturedNewsCard
 import my.android.newsapp.ui.component.LoadingScreen
+import my.android.newsapp.ui.component.NewsCard
 import my.android.newsapp.ui.theme.NewsAppTheme
 import my.android.newsapp.ui.viewmodel.HomeViewmodel
 import my.android.newsapp.ui.viewmodel.NewsUiState
@@ -95,6 +94,7 @@ fun NewsScreen(
 ) {
     var currentCategory by remember { mutableStateOf(selectedCategory) }
     var currentCountry by remember { mutableStateOf(selectedCountry) }
+    var focusedArticle by remember { mutableStateOf(articles.firstOrNull()) }
 
     val categories =
         listOf("General", "Entertainment", "Business", "Health", "Technology", "Sports", "Science")
@@ -109,137 +109,145 @@ fun NewsScreen(
 
     var showDropdown by remember { mutableStateOf(false) }
 
-    Column(
+    Box(
         modifier = Modifier
             .fillMaxSize()
-            .padding(horizontal = 32.dp)
+            .background(MaterialTheme.colorScheme.background)
     ) {
-        Spacer(Modifier.height(12.dp))
-        Row (modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically){
-            Image(
-                painter = painterResource(R.drawable.ic_tv),
-                contentDescription = "App Icon"
-            )
-            Spacer(Modifier.width(12.dp))
-            Text(
-                text = "NewsTV",
-                textAlign = TextAlign.Center,
-                style = MaterialTheme.typography.headlineMedium
+        focusedArticle?.let { article ->
+            FeaturedNewsCard(
+                modifier = Modifier.fillMaxSize(),
+                article = article
             )
         }
-        Spacer(Modifier.height(24.dp))
-        Row(
+
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 16.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceBetween
+                .fillMaxSize()
+                .padding(horizontal = 32.dp)
         ) {
-            TabCategorySelector(
-                categories = categories,
-                selectedCategory = categories.indexOf(currentCategory),
-                onCategorySelected = { index ->
-                    currentCategory = categories[index]
-                    viewModel.updateCategory(categories[index])
-                },
-                onCategoryFocus = { focusIndex ->
-                    currentCategory = categories[focusIndex]
-                }
-            )
+            Spacer(Modifier.height(12.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.ic_tv),
+                    contentDescription = "App Icon"
+                )
+                Spacer(Modifier.width(12.dp))
+                Text(
+                    text = "NewsTV",
+                    textAlign = TextAlign.Center,
+                    style = MaterialTheme.typography.headlineMedium
+                )
+            }
+            Spacer(Modifier.height(24.dp))
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                TabCategorySelector(
+                    categories = categories,
+                    selectedCategory = categories.indexOf(currentCategory),
+                    onCategorySelected = { index ->
+                        currentCategory = categories[index]
+                        viewModel.updateCategory(categories[index])
+                    },
+                )
 
-            Box {
-                IconButton(
-                    onClick = { showDropdown = !showDropdown },
-                    modifier = Modifier
-                        .size(28.dp)
-                        .focusable()
-                ) {
-                    Icon(
-                        imageVector = Icons.Default.Settings,
-                        contentDescription = "Settings",
-                        modifier = Modifier.size(24.dp)
-                    )
-                }
+                Box {
+                    IconButton(
+                        onClick = { showDropdown = !showDropdown },
+                        modifier = Modifier.size(28.dp)
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Settings,
+                            contentDescription = "Settings",
+                            modifier = Modifier.size(24.dp)
+                        )
+                    }
 
-                DropdownMenu(
-                    expanded = showDropdown,
-                    onDismissRequest = { showDropdown = false },
-                    containerColor = MaterialTheme.colorScheme.surface,
-                    modifier = Modifier.align(Alignment.TopEnd)
-                ) {
-                    DropdownMenuItem(
-                        text = {
-                            Text(
-                                "Change Country",
-                                color = MaterialTheme.colorScheme.onSurfaceVariant
-                            )
-                        },
-                        leadingIcon = {
-                            Icon(
-                                painter = painterResource(R.drawable.ic_public),
-                                contentDescription = null,
-                                tint = MaterialTheme.colorScheme.onSurfaceVariant
-                            )
-                        },
-                        onClick = { },
-                    )
-                    HorizontalDivider()
-                    countryWithCodes.forEach { (country, code) ->
+                    DropdownMenu(
+                        expanded = showDropdown,
+                        onDismissRequest = { showDropdown = false }
+                    ) {
                         DropdownMenuItem(
-                            text = {
-                                Text(
-                                    country,
-                                    color = MaterialTheme.colorScheme.onSurfaceVariant,
-                                    textAlign = TextAlign.Center,
-                                    modifier = Modifier.fillMaxWidth()
+                            text = { Text("Change Country") },
+                            leadingIcon = {
+                                Icon(
+                                    painter = painterResource(R.drawable.ic_public),
+                                    contentDescription = null
                                 )
                             },
-                            onClick = {
-                                currentCountry = country
-                                viewModel.updateCountry(code)
-                                showDropdown = false
-                            }
+                            onClick = {}
                         )
+                        HorizontalDivider()
+                        countryWithCodes.forEach { (country, code) ->
+                            DropdownMenuItem(
+                                text = { Text(country) },
+                                onClick = {
+                                    currentCountry = country
+                                    viewModel.updateCountry(code)
+                                    showDropdown = false
+                                }
+                            )
+                        }
                     }
                 }
             }
-        }
-        Spacer(Modifier.height(24.dp))
-        if (articles.isEmpty()) {
-            EmptyResult()
-        } else {
-            HeadlinesList(articles)
+
+            if (articles.isEmpty()) {
+                EmptyResult()
+            } else {
+                Spacer(modifier = Modifier.weight(1f))
+                Column {
+                    Text("Top Headlines", style = MaterialTheme.typography.titleMedium)
+                    HeadlinesList(articles) { focusedArticle = it }
+                }
+            }
         }
     }
 }
 
-@RequiresApi(Build.VERSION_CODES.O)
 @Composable
-fun HeadlinesList(articles: List<Article>) {
-    LazyColumn {
+fun HeadlinesList(
+    articles: List<Article>,
+    onArticleFocus: (Article) -> Unit
+) {
+    LazyRow(horizontalArrangement = Arrangement.spacedBy(24.dp)) {
         items(articles) { article ->
-            HeadlineCard(article)
+            if (article.title != "[Removed]")
+                NewsCard(
+                    article = article,
+                    modifier = Modifier,
+                    onFocusChange = { isFocused ->
+                        if (isFocused) {
+                            onArticleFocus(article)
+                        }
+                    },
+                )
         }
     }
 }
 
-@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun TabCategorySelector(
     categories: List<String>,
     selectedCategory: Int,
     onCategorySelected: (Int) -> Unit,
-    onCategoryFocus: (Int) -> Unit
 ) {
-    TabRow(selectedTabIndex = selectedCategory, modifier = Modifier.focusRestorer()) {
+    TabRow(selectedTabIndex = selectedCategory) {
         categories.forEachIndexed { index, tab ->
             key(index) {
                 Tab(
                     selected = index == selectedCategory,
-                    onFocus = { onCategoryFocus(index) },
-                    onClick = { onCategorySelected(index) }
+                    onClick = { onCategorySelected(index) },
+                    selectedContentColor = Color.Black
                 ) {
                     Text(
                         text = tab,
@@ -258,12 +266,10 @@ fun EmptyResult() {
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            Text(
-                text = "No news headlines found for this combination of news category and country " +
-                        "please try some other combination...",
-                style = MaterialTheme.typography.bodyMedium
-            )
-        }
+        Text(
+            text = "No news headlines found for this combination of news category and country " +
+                    "please try some other combination...",
+            style = MaterialTheme.typography.bodyMedium
+        )
     }
 }
